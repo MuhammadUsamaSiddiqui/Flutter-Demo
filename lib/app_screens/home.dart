14b441a870f96a622471af4b20c23e8a3a622c47
usama_siddiqui <usama.siddiqui@technado.co>
2019-12-30 18:20:10 +0500

    11 to 17 videos

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,31 +1,115 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Center(
+        // Center widget because of Box Constraints
 
-    return Center(   // Center widget because of Box Constraints
-      child: Container(   // Container equal to div element in HTML
-          alignment: Alignment.center,
-          color: Colors.deepPurple,
+        child: Container(
+            // Container equal to div element in HTML
+
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
 //          width: 200.0,
 //          height: 100.0,
-          margin: EdgeInsets.only(left : 15.0, right: 15.0),   // padding also apply in the same manner
+            margin: EdgeInsets.only(left: 15.0, right: 15.0),
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            // padding also apply in the same manner
+
+            child: Column(children: <Widget>[
+              Row(children: <Widget>[
+                Expanded(
+                    child: Text("Spice Jet",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 35.0,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white))),
+                Expanded(
+                    child: Text("From Karachi to Lahore",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white)))
+              ]),
+              Row(children: <Widget>[
+                Expanded(
+                    child: Text("PIA",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 35.0,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white))),
+                Expanded(
+                    child: Text("From Islamabad to Lahore",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white)))
+              ]),
+              GoogleImageAssets(),
+              BookingButton()
+            ])));
+  }
+}
+
+// For Image
+class GoogleImageAssets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/download.png');
+    Image image = Image(image: assetImage, width: 250.0, height: 250.0);
+    return Container(child: image);
+  }
+}
+
+// For Raised Button
+class BookingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.deepOrange,
           child: Text(
-            "Flight",
-            textDirection: TextDirection.ltr,
+            "Book your Flight",
             style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 35.0,
-              fontFamily: "Raleway",
-              fontWeight: FontWeight.w300,
-              fontStyle: FontStyle.italic,
-              color: Colors.white
-            ),
-          )
-      ),
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: "Raleway",
+                fontWeight: FontWeight.w700),
+          ),
+          elevation: 6.0,
+          onPressed: () => bookFlight(context)),
     );
   }
 
+  // For Alert Dialog
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+        title: Text("Booked Successfully"),
+        content: Text("Have a Pleasant Flight"));
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) =>
+            alertDialog); // here builder is a function handler
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,82 @@ class MyFlutterApp extends StatelessWidget{  // the widget that will be returned
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app_screens/home.dart';
 
-void main (){
-
-  runApp(
-      MaterialApp(
-        title: "Section 2",
-        home: Home(),
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Section 2",
+    home: Scaffold(
+      // here we used Scaffold because we can't directly assign list view to Material App home
+      appBar: AppBar(
+        title: Text("Listview"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: getLongListView(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            debugPrint("FAB clicked");
+          },
+        child: Icon(Icons.add),
+        tooltip: 'Add one more item'
       )
+    )
+  ));
+}
+
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "item $counter");
+  return items;
+}
+
+Widget getLongListView() {
+
+  var listItems = getListElements();
+
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    // itemBuilder plays an important role in creating our lis items
+    return ListTile(
+      title: Text(listItems[index]),
+      leading: Icon(Icons.arrow_right),
+      onTap: (){
+        //debugPrint("${listItems[index]} was tapped");
+        showSnackbar(context, listItems[index]);
+      }
+    );
+  });
+
+  return listView;
+}
+
+void showSnackbar(BuildContext context, String listItem){
+
+  var snackbar = SnackBar(
+    content: Text("You just tapped the $listItem"),
+    action: SnackBarAction(
+      label: "UNDO",
+      onPressed: (){
+        debugPrint('Performing Dummy undo option');
+      }
+    )
   );
+
+  Scaffold.of(context).showSnackBar(snackbar);
+}
+
+// Basic ListView
+Widget getListView() {
+  var listView = ListView(children: <Widget>[
+    ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("Landscape Tapped");
+        }),
+    ListTile(leading: Icon(Icons.laptop_chromebook), title: Text("Windows")),
+    ListTile(leading: Icon(Icons.phone), title: Text("Phone")),
+    Text(" Another element in the listview"),
+    Container(color: Colors.red, height: 50.0)
+  ]);
+  return listView;
 }
